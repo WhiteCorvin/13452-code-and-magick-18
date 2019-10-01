f7d42ed0fcc8179515194d7f9140c94928f96713
Pavel Petrov <pavelpetrov7wc@gmail.com>
2019-10-01 20:07:49 +0700

    Переделал модуль util

--- js/utils.js
+++ js/utils.js
@@ -1,66 +1,82 @@
 'use strict';
 
 (function () {
+
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
-  var util = {
-    doActionIfEscEvent: function (evt, action) {
-      if (evt.keyCode === ESC_KEYCODE) {
-        action();
-      }
-    },
-    doActionIfEnterEvent: function (evt, action) {
-      if (evt.keyCode === ENTER_KEYCODE) {
-        action();
-      }
-    },
-    getRandomInt: function (max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    },
-    getRandomData: function (arr) {
-      var position = util.getRandomInt(arr.length);
-      return arr[position];
-    },
-    getColor: function (arr, position) {
-      return arr[position];
-    },
-    getColorForObject: function (object, array, count, input) {
-      var colorObject = util.getColor(array, count);
 
-      object.style.fill = colorObject;
-      input.value = colorObject;
-
-      if (count === (array.length - 1)) {
-        count = 0;
-      } else {
-        count++;
-      }
-      return count;
-    },
-    getBackgorundColorForObject: function (object, array, count, input) {
-      var colorObject = util.getColor(array, count);
-
-      object.style.backgroundColor = colorObject;
-      input.value = colorObject;
-
-      if (count === (array.length - 1)) {
-        count = 0;
-      } else {
-        count++;
-      }
-      return count;
-    },
-    getMaxElement: function (arr) {
-      var maxElement = arr[0];
-
-      for (var i = 1; i < arr.length; i++) {
-        if (arr[i] > maxElement) {
-          maxElement = arr[i];
-        }
-      }
-
-      return maxElement;
+  var doActionIfEscEvent = function (evt, action) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      action();
     }
   };
-  window.util = util;
+
+  var doActionIfEnterEvent = function (evt, action) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      action();
+    }
+  };
+
+  var getRandomInt = function (max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+
+  var getRandomData = function (arr) {
+    var position = getRandomInt(arr.length);
+    return arr[position];
+  };
+
+  var getColor = function (arr, position) {
+    return arr[position];
+  };
+
+  var getColorForObject = function (object, array, count, input) {
+    var colorObject = getColor(array, count);
+
+    object.style.fill = colorObject;
+    input.value = colorObject;
+
+    if (count === (array.length - 1)) {
+      count = 0;
+    } else {
+      count++;
+    }
+    return count;
+  };
+
+  var getBackgorundColorForObject = function (object, array, count, input) {
+    var colorObject = getColor(array, count);
+
+    object.style.backgroundColor = colorObject;
+    input.value = colorObject;
+
+    if (count === (array.length - 1)) {
+      count = 0;
+    } else {
+      count++;
+    }
+    return count;
+  };
+
+  var getMaxElement = function (arr) {
+    var maxElement = arr[0];
+
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
+        maxElement = arr[i];
+      }
+    }
+
+    return maxElement;
+  };
+
+  window.util = {
+    doActionIfEscEvent: doActionIfEscEvent,
+    doActionIfEnterEvent: doActionIfEnterEvent,
+    getRandomData: getRandomData,
+    getColorForObject: getColorForObject,
+    getBackgorundColorForObject: getBackgorundColorForObject,
+    getMaxElement: getMaxElement
+  };
+
 })();
